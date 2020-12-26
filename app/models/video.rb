d2c79c155edafd72b04a38527bea2811273059a9
Eli Harrison <admin@eliharrison.com>
2020-12-25 18:25:42 -0800

    Added basic mime type, but it doesn't recognize '.mkv' extension.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video
 
+
   # add this last.
   # has_one_attached :recording
 
@@ -19,11 +20,12 @@ class Video
   end
 
 
-  def source_attachment_path
+  def source_attachment_file_path
   end
 
 
-  def source_catalog_path
+  def source_catalog_file_path
+    'yourallgay.mkv'
   end
 
 
@@ -31,10 +33,37 @@ class Video
   end
 
 
+  def source_file_extname
+    File::extname(source_file_path.to_s)
+  end
+
+
+  def source_file_extension
+    source_file_extname.to_s.gsub(/\A./,'')
+  end
+
+
+  def source_file_mime_type
+    Mime::Type.lookup_by_extension(source_file_extension)
+  end
+
+
+  def source_file_path
+    case source_type
+    when :attachment
+      source_attachment_file_path
+    when :catalog
+      source_catalog_file_path
+    end
+  end
+
+
   def source_type
     # :attachment
     # :catalog
     # :embed
+
+    :catalog
   end
 
 
