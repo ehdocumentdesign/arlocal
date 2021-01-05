141f845e7f2213cd632858b8a986bd078a784666
Eli Harrison <admin@eliharrison.com>
2021-01-04 21:15:14 -0800

    Refactored views/admin/arlocal_settings/* with shared form_elements

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -6,10 +6,19 @@ module FormHelper
   end
 
 
+  def form_message_picture_datetime_cascade
+    "The 'datetime cascade' option will order pictures based on the first available data, choosing from\n1) a manually entered date/time, 2) the date/time from EXIF metadata, 3) the date/time from the picture file."
+  end
+
+
+  def form_message_slug_description
+    'A&R.local will automatically generate a URL-friendly "slug" for albums, events, pictures, and keywords. This option, when checked, will enable a text field to edit the "slug" of a resource.'
+  end
+
+
   def form_label_without_top_level(symbol)
     symbol.to_s.split('_')[1..-1].join('_').to_sym
   end
-
 
 
 end
