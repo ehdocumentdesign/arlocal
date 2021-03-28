6bdebcaa4cacdc4d73d2d8f8e9fb890b91f8f912
Eli Harrison <admin@eliharrison.com>
2021-03-28 11:29:54 -0700

    Probably obsolete.

--- app/env/arlocal_env.rb
+++ app/env/arlocal_env.rb
@@ -1,32 +1,32 @@
 module ArlocalEnv
 
 
-  module_function
-
-
-  def app_copyright_year
-    2021
-  end
-
-
-  def app_version
-    '0.19'
-  end
-
-
-  def app_logo_file_path
-    '/arlocal/arlocal-logo.png'
-  end
-
-
-  def app_logo_icon_file_path
-    '/arlocal/arlocal-logo-icon.png'
-  end
-
-
-  def app_nilpicture_file_path
-    '/arlocal/arlocal-nilpicture.png'
-  end
+  # module_function
+  #
+  #
+  # def app_copyright_year
+  #   2021
+  # end
+  #
+  #
+  # def app_version
+  #   '0.19'
+  # end
+  #
+  #
+  # def app_logo_file_path
+  #   '/arlocal/arlocal-logo.png'
+  # end
+  #
+  #
+  # def app_logo_icon_file_path
+  #   '/arlocal/arlocal-logo-icon.png'
+  # end
+  #
+  #
+  # def app_nilpicture_file_path
+  #   '/arlocal/arlocal-nilpicture.png'
+  # end
 
 
 end
