cb9a7541a6dd680b0cada75649dcbf3ac84d9d9f
Eli Harrison <admin@eliharrison.com>
2021-09-16 19:17:05 -0700

    Refactored ParsersHelper methods.

--- app/helpers/parsers_helper.rb
+++ app/helpers/parsers_helper.rb
@@ -1,30 +1,49 @@
 module ParsersHelper
 
 
-  # parser_div() returns the html_class of the parser
-  # therefore this method is not as easy to DRY up by calling parser_result from within
   def parser_div(resource_text_props)
-    parser = MarkupParser.find(resource_text_props[:parser_id])
-    text_markup = resource_text_props[:text_markup]
-    parsed_text = parser.parse(text_markup)
-    sanitized_text = sanitize(parsed_text)
-    tag.div(sanitized_text, class: parser.html_class)
+    parser_result(resource_text_props, wrap_with: :div)
   end
 
 
-  def parser_remove_markup(resource_text_props)
-    strip_tags(parser_result(resource_text_props)).strip
+  def parser_result(resource_text_props, html_class: nil, wrap_with: nil)
+    parser_result = MarkupParser.parse_sanitize_class(resource_text_props)
+    case wrap_with
+    when :div
+      content_tag(wrap_with, parser_result[:sanitized_text], class: parser_result[:html_class])
+    when nil
+      parser_result[:sanitized_text]
+    end
   end
 
 
-  # parser_result() is html-agnostic, it does not return a tag or a class
-  # therefore this method is not as easy to DRY up by calling parser_div
-  def parser_result(resource_text_props)
-    parser = MarkupParser.find(resource_text_props[:parser_id])
-    text_markup = resource_text_props[:text_markup]
-    parsed_text = parser.parse(text_markup)
-    sanitize(parsed_text)
-  end
+
+
+
+  # # parser_div() returns the html_class of the parser
+  # # therefore this method is not as easy to DRY up by calling parser_result from within
+  # def parser_div(resource_text_props)
+  #   parser = MarkupParser.find(resource_text_props[:parser_id])
+  #   parsed_text = parser.parse(resource_text_props[:text_markup])
+  #   sanitized_text = sanitize(parsed_text)
+  #   tag.div(sanitized_text, class: parser.html_class)
+  # end
+  #
+  #
+  # def parser_remove_markup(resource_text_props)
+  #   strip_tags(parser_result(resource_text_props)).strip
+  # end
+  #
+  #
+  # # parser_result() is html-agnostic, it does not return a tag or a class
+  # # therefore this method is not as easy to DRY up by calling parser_div
+  # def parser_result(resource_text_props)
+  #   parser = MarkupParser.find(resource_text_props[:parser_id])
+  #   parsed_text = parser.parse(resource_text_props[:text_markup])
+  #   sanitize(parsed_text)
+  # end
+  #
+
 
 
 end
--- app/models/markup_parser.rb
+++ app/models/markup_parser.rb
@@ -6,31 +6,36 @@ class MarkupParser
     {
       id: 0,
       description: 'No formatting',
-      method: lambda { |text| text.to_s },
+      method_parse: lambda { |text| text.to_s },
+      method_sanitize: lambda { |text| ApplicationController.helpers.sanitize(text) },
       symbol: :no_formatting
     },
     {
       id: 1,
       description: 'HTML Paragraph',
-      method: lambda { |text| ApplicationController.helpers.tag.p(text.to_s) },
+      method_parse: lambda { |text| ApplicationController.helpers.tag.p(text.to_s) },
+      method_sanitize: lambda { |text| ApplicationController.helpers.sanitize(text) },
       symbol: :html_p
     },
     {
       id: 2,
       description: 'HTML Preformatted',
-      method: lambda { |text| "<pre>\n" + text.to_s + "\n</pre>" },
+      method_parse: lambda { |text| "<pre>\n" + text.to_s + "\n</pre>" },
+      method_sanitize: lambda { |text| ApplicationController.helpers.sanitize(text) },
       symbol: :html_pre
     },
     {
       id: 3,
       description: 'Markdown – Commonmarker',
-      method: lambda { |text| CommonMarker.render_html(text.to_s) },
+      method_parse: lambda { |text| CommonMarker.render_html(text.to_s) },
+      method_sanitize: lambda { |text| ApplicationController.helpers.sanitize(text) },
       symbol: :markdown_commonmarker
     },
     {
       id: 4,
       description: 'Simple Format – Rails',
-      method: lambda { |text| ApplicationController.helpers.simple_format(text.to_s) },
+      method_parse: lambda { |text| ApplicationController.helpers.simple_format(text.to_s) },
+      method_sanitize: lambda { |text| ApplicationController.helpers.sanitize(text) },
       symbol: :simple_format_rails
     }
   ]
@@ -38,17 +43,12 @@ class MarkupParser
   HTML_CLASS_PREFIX = 'arl_markup_parser'
 
 
-  attr_reader :id, :description, :html_class, :method, :symbol
+  protected
 
 
-  def initialize(parser)
-    if parser
-      @id = parser[:id]
-      @description = parser[:description]
-      @html_class = [ MarkupParser::HTML_CLASS_PREFIX, parser[:symbol].to_s ].join('_')
-      @method = parser[:method]
-      @symbol = parser[:symbol]
-    end
+  def self.parse_sanitize_class(resource_text_props)
+    parser = self.find(resource_text_props[:parser_id])
+    { html_class: parser.html_class, sanitized_text: parser.parse_and_sanitize(resource_text_props[:text_markup]) }
   end
 
 
@@ -56,14 +56,35 @@ class MarkupParser
   public
 
 
-  def call(text)
-    @method.call(text)
+  attr_reader :id, :description, :html_class, :method_parse, :method_sanitize, :symbol
+
+
+  def initialize(parser)
+    if parser
+      @id = parser[:id]
+      @description = parser[:description]
+      @html_class = [ MarkupParser::HTML_CLASS_PREFIX, parser[:symbol].to_s ].join('_')
+      @method_parse = parser[:method_parse]
+      @method_sanitize = parser[:method_sanitize]
+      @symbol = parser[:symbol]
+    end
   end
 
 
   def parse(text)
-    call(text)
+    @method_parse.call(text)
   end
+
+
+  def parse_and_sanitize(text)
+    sanitize(parse(text))
+  end
+
+
+  def sanitize(text)
+    @method_sanitize.call(text)
+  end
+
 
 
 end
