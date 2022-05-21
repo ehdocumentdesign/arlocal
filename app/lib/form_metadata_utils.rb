4b035a68239d6439077b592b2fd53b8203c5fba8
Eli Harrison <admin@eliharrison.com>
2022-05-21 00:32:22 -0700

    Modified visual formatting.

--- app/lib/form_metadata_utils.rb
+++ app/lib/form_metadata_utils.rb
@@ -3,9 +3,9 @@ module FormMetadataUtils
 
   def navbar_categories
     self::DATA.select  { |k,v| v[:navbar] != nil }.
-    sort_by { |k,v| k.to_s.downcase }.
-    sort_by { |k,v| v[:navbar] }.
-    map     { |k,v| k }
+               sort_by { |k,v| k.to_s.downcase }.
+               sort_by { |k,v| v[:navbar] }.
+               map     { |k,v| k }
   end
 
 
